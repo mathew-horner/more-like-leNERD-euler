16f166e7a8a85c4fdb425c6c9c081cad46368980
mdx97 <mathewhorner456@gmail.com>
2025-10-30 15:13:03 -0500

    Add command line argument for problem number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 use std::collections::HashSet;
+use std::{env, process};
 
 fn main() {
-    println!("{}", quadratic_primes());
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: cargo run -- <PROBLEM NUMBER>");
+        process::exit(1);
+    }
+
+    let problem_number: usize = args[1].parse().unwrap();
+    execute(problem_number);
+}
+
+fn execute(problem_number: usize) {
+    match problem_number {
+        1 => println!("{}", multiples_of_3_or_5()),
+        2 => println!("{}", even_fibonacci_numbers()),
+        3 => println!("{}", quadratic_primes()),
+        other => eprintln!("no solution for problem {other}"),
+    }
 }
 
 /// Problem 1 - Find the number of multiples of 3 or 5 under 1000.
